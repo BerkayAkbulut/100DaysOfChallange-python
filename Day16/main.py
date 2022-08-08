9b653584dddc0a2c0549f64243f11c226394005c
Berkay Akbulut <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-08-08 22:27:18 +0300

    day19

--- Day16/main.py
+++ Day16/main.py
@@ -1,9 +1,16 @@
-import turtle
-from turtle import Turtle,Screen
+#from turtle import Turtle,Screen
+#
+#timmy = Turtle()
+#timmy.shape("turtle")
+#timmy.color("coral")
+#timmy.forward(100)#
 
+#my_screen =Screen()
+#my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Names",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
 
-my_screen =Screen()
-my_screen.exitonclick()
+print(table)
