4eed09b244b75a600f6f71026c9d1a6567e9d601
Berkay Akbulut <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-08-15 22:58:24 +0300

    just a break (22)

--- Day22/paddle.py
+++ Day22/paddle.py
@@ -1,44 +1,18 @@
 from turtle import Turtle
 
-STARTING_POSİTİONS = [(350, 40), (350, 20), (350, 0), (350, -20), (350, -40)]
-UP = 20
-DOWN = 20
-MOVE_DISTANCE=20
 
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        paddle = Turtle()
+        paddle.shape("square")
+        paddle.color("white")
+        paddle.shapesize(stretch_wid=5, stretch_len=1)
+        paddle.penup()
+        paddle.goto(position)
 
-class Paddle():
-    def __init__(self):
-        self.paddle = []
-        self.creat_padlle()
-        self.head_of_paddle = self.paddle[0]
+    def go_up(self):
+        self.goto(350, self.ycor() + 20)
 
-    def creat_padlle(self):
-        for position in STARTING_POSİTİONS:
-            self.add_cell(position)
-
-    def add_cell(self, position):
-        paddle_object = Turtle(shape=("square"))
-        paddle_object.speed(1)
-        paddle_object.penup()
-        paddle_object.color("white")
-        paddle_object.goto(position)
-        self.paddle.append(paddle_object)
-
-    def move(self):
-        for paddle_num in range(len(self.paddle) - 1, 0, -1):
-            new_x = self.paddle[paddle_num - 1].xcor()
-            new_y = self.paddle[paddle_num - 1].ycor()
-            print(f"x:{new_x} y:{new_y}")
-            self.paddle[paddle_num].goto(x=new_x, y=new_y)
-
-        self.head_of_paddle.setheading(90)
-        self.head_of_paddle.forward(MOVE_DISTANCE)
-
-    def up(self):
-        self.head_of_paddle.setheading(90)
-        self.head_of_paddle.forward(UP)
-        self.move()
-    def down(self):
-        self.head_of_paddle.backward(DOWN)
-        self.head_of_paddle.setheading(90)
-        self.move()
+    def go_down(self):
+        self.goto(350, self.ycor() - 20)
