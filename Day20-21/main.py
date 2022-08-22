73ab625cad15c04d14bb2b8059be23c8b0462567
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-08-22 18:45:30 +0300

    Day24 Upgrade
    I added best score info to SnakaGame

--- Day20-21/main.py
+++ Day20-21/main.py
@@ -4,46 +4,42 @@ from snake import Snake
 from food import Food
 from scoreboard import ScoreBoard
 
-screen=Screen()
-screen.setup(width=600,height=600)
+screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake=Snake()
+snake = Snake()
 food = Food()
 score = ScoreBoard(screen)
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-
-game_is_on=True
+game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    if snake.head_of_snake.distance(food) <15:
+    if snake.head_of_snake.distance(food) < 15:
         food.reflesh_coor()
         snake.extend()
         score.reflesh_score()
 
-    if snake.head_of_snake.xcor() > 265 or snake.head_of_snake.xcor()<-280 or snake.head_of_snake.ycor() >280 or snake.head_of_snake.ycor() < -265:
-        game_is_on=False
+    if snake.head_of_snake.xcor() > 265 or snake.head_of_snake.xcor() < -280 or snake.head_of_snake.ycor() > 280 or \
+            snake.head_of_snake.ycor() < -265:
+        game_is_on = False
         score.game_over()
 
     for cell in snake.snake[1:]:
-        if snake.head_of_snake.distance(cell)<10:
-            game_is_on=False
+        if snake.head_of_snake.distance(cell) < 10:
+            game_is_on = False
             score.game_over()
 
     snake.move()
 
 screen.exitonclick()
-
-
-
-
--- Day20-21/scoreboard.py
+++ Day20-21/scoreboard.py
@@ -5,21 +5,31 @@ class ScoreBoard(Turtle):
     def __init__(self,screen):
         super().__init__()
         self.score=0
+        self.high_score =0
+        with open("data.txt", mode="r") as f:
+            self.high_score = f.read()
+        #f.close()
         # self.clear()
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(arg=self.score,move=False,align="center",font=("Courier",24 , "normal"))
+        self.write(arg=f"Score: {self.score} | High score: {self.high_score}",move=False,align="center",font=("Courier",24 , "normal"))
         self.hideturtle()
 
     def reflesh_score(self):
         self.score+=1
         self.clear()
-        self.write(arg=self.score, move=False, align="center", font=("Arial", 24, "normal"))
+        self.write(arg=f"Score: {self.score} | High score: {self.high_score}",move=False,align="center",font=("Courier",24 , "normal"))
 
     def game_over(self):
-        #self.clear()
+        with open("data.txt", mode="r+") as ff:
+            if self.score>int(self.high_score):
+                ff.write(str(self.score))
+            else:
+                ff.write(int(self.high_score))
+            #ff.close()
+
         self.color("white")
         self.penup()
         self.goto(0, 0)
-        self.write(arg="GAME OVER!", move=False, align="center", font=("Courier", 24, "normal"))
+        self.write(arg=f"Score: {self.score} | High score: {self.high_score}",move=False,align="center",font=("Courier",24 , "normal"))
