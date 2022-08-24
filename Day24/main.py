9c6abe4ab226317f465ff432cd7d7e8a3931a5fb
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-08-24 16:23:52 +0300

    Day24 done

--- Day24/main.py
+++ Day24/main.py
@@ -1,8 +1,26 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("./Input/Names/invited_names.txt", 'r') as invited_names:
+    names = invited_names.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_context:
+    letter = letter_context.read()
+
+# print(letter.replace("[name]","berkay"))
+
+for name in names:
+    with open("./Input/Letters/starting_letter.txt") as letter_context:
+        letter = letter_context.read()
+    name=name.replace("\n","")
+    letter=letter.replace("[name]",name)
+    print(letter)
+    with open(f'./Output/ReadyToSend/letter_for_{name}.txt', 'w') as f:
+        f.write(letter)
