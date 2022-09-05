2da48eebecb93abc2bab4c4b41b530ead7c3bf2b
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-09-05 17:42:36 +0300

    day27some codes

--- Day27/main.py
+++ Day27/main.py
@@ -1,35 +1,25 @@
 from tkinter import *
 
-window = Tk()
-window.title("my first GUI program")
-window.minsize(width=400, height=400)
-
-entry_string = "I am a label"
-my_label = Label(text=entry_string , font=("Arial", 24))
-my_label.pack()  # pack() modülü ortaya yazılmasını sağlıyor
-
-
-# Button
 
 def clicked_button():
     entry_string = input.get()
     my_label.config(text=entry_string)
 
 
+window = Tk()
+window.title("my first GUI program")
+window.minsize(width=400, height=400)
+
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24))
+my_label.pack()  # pack() modülü ortaya yazılmasını sağlıyor
+
+# Button
 button = Button(text="Click Me.", command=clicked_button)
 button.pack()
 
 # Entry
-
 input = Entry()
 input.pack()
-
-
-
-
-
-
-
-
 
 window.mainloop()
