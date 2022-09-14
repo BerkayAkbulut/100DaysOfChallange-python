352ce58961e7fd827f6f92530bfcbe6bcea8952a
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-09-14 17:54:57 +0300

    day28 some changes

--- Day28/pomodoro-start/main.py
+++ Day28/pomodoro-start/main.py
@@ -6,21 +6,43 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 2
+SHORT_BREAK_MIN = 2
+LONG_BREAK_MIN = 5
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    canvas.itemconfig(timer_text, text="00:00")
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def get_started():
-    count_down(5*60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+
+    # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=f"{int(count/60)}:{count%60}")
+    sec_count = count % 60
+    min_count = int(count / 60)
+
+    if sec_count < 10:
+        sec = '0' + str(sec_count)
+
+    canvas.itemconfig(timer_text, text=f"{min_count}:{sec_count}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -41,7 +63,7 @@ canvas.grid(column=1, row=1)
 start = Button(text="Start", highlightthickness=0, command=get_started)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset", highlightthickness=0)
+reset = Button(text="Reset", highlightthickness=0, command=reset)
 reset.grid(column=2, row=2)
 
 checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
