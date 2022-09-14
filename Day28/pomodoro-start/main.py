71ff3bc36ce6638ec4202e7689ac131b3a82f708
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-09-14 16:04:16 +0300

    day28-count is available

--- Day28/pomodoro-start/main.py
+++ Day28/pomodoro-start/main.py
@@ -10,23 +10,44 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def get_started():
+    count_down(5*60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    canvas.itemconfig(timer_text, text=f"{int(count/60)}:{count%60}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100,pady=50,bg=YELLOW)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224 ,bg=YELLOW, highlightthickness=0)
-tomato_img=PhotoImage(file="tomato.png")
-canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130, text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+# count_down(5)
 
+start = Button(text="Start", highlightthickness=0, command=get_started)
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", highlightthickness=0)
+reset.grid(column=2, row=2)
+
+checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark.grid(column=1, row=3)
+
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer.grid(column=1, row=0)
 
 window.mainloop()
