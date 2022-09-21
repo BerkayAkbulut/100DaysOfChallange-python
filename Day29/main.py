5bdcd41258a32013131dc1c08134b7f9ea88dde4
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-09-21 16:46:37 +0300

    Day29 done

--- Day29/main.py
+++ Day29/main.py
@@ -1,8 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    input_password.delete(0, 'end')
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    pasword_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = password_letters + password_symbols + pasword_numbers
+
+    random.shuffle(password_list)
+
+    passwordd = "".join(password_list)
+    pyperclip.copy(passwordd)
+    input_password.insert(0, passwordd)
+    # print(f"Your password is: {passwordd}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -36,27 +62,36 @@ canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
 # LABELS
-label_web_site = Label(text='Website :')
-label_web_site.grid(column=0, row=1)
-label_email = Label(text="E-mail/Username :")
-label_email.grid(column=0, row=2)
-label_password = Label(text="Password :")
-label_password.grid(column=0, row=3)
+
+
+label_web_site = Label(text="Website : ")
+label_web_site.grid(row=1, column=0)
+
+label_email = Label(text="Email / Username : ")
+label_email.grid(row=2, column=0)
+
+label_password = Label(text="Password : ")
+label_password.grid(row=3, column=0)
 
 # INPUTS
+# Entries
 input_website = Entry(width=35)
-input_website.grid(column=1, row=1, columnspan=2)
+input_website.grid(row=1, column=1, columnspan=2)
 input_website.focus()
+
 input_email = Entry(width=35)
-input_email.grid(column=1, row=2, columnspan=2)
-input_email.insert(0, "akbulutberkay@yandex.com")
-input_password = Entry(width=17)
-input_password.grid(column=1, row=3)
+input_email.grid(row=2, column=1, pady=5, columnspan=2)
+input_email.insert(0, "email@example.com")
+
+input_password = Entry(width=21)
+input_password.grid(row=3, column=1)
 
 # BUTTONS
-button_gen_pass = Button(text="Generate Password")
-button_gen_pass.grid(column=2, row=3)
-button_add = Button(text="Add", width=36, command=save)
-button_add.grid(column=1, row=4)
+# Buttons
+button_gen_pass = Button(text="Generate Password", command=generate_password)
+button_gen_pass.grid(row=3, column=2)
+
+add_button = Button(text="Add Password", width=36, command=save)
+add_button.grid(row=4, column=1, pady=5, columnspan=2)
 
 window.mainloop()
