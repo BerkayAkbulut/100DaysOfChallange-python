05c048555f97093d0ab4088c5959c1f94cc53b54
Berkay Akbulut <berkayakbulut@Berkays-MacBook-Pro.local>
2022-11-18 23:18:34 +0300

    Day 31 done

--- Day31/main.py
+++ Day31/main.py
@@ -8,7 +8,6 @@ WORD = "word"
 
 current_card = {}
 
-
 # ------------TIMING--------------------------
 
 def newWord():
@@ -28,7 +27,14 @@ def flip_card():
     canvas.itemconfig(lang_of_text, text="Türkçe", fill="white")
     canvas.itemconfig(word_text, text=current_card["Turkish"], fill="white")
 
+def is_know():
+    df_list.remove(current_card)
+    print(len(df_list))
 
+    data=pd.DataFrame(df_list)
+    data.to_csv('data/known_words.csv', index=False)
+
+    newWord()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flash Card")
@@ -54,12 +60,17 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, command=newWord)
 wrong_button.grid(column=0, row=1, padx=50, pady=20)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=newWord)
+right_button = Button(image=right_image, highlightthickness=0, command=is_know)
 right_button.grid(column=1, row=1, padx=50, pady=20)
 
 # Read data
-df = pd.read_csv("data/english_words.csv")
-df_list = df.to_dict(orient="records")
+try:
+    df = pd.read_csv("data/known_words.csv")
+except FileNotFoundError:
+    original_df = pd.read_csv("data/english_words copy.csv")
+    df_list = original_df.to_dict(orient="records")
+else:
+    df_list = df.to_dict(orient="records")
 
 newWord()
 # window.after(3000,newWord_Tr(current_card))
