fdac84ee9f26171035c8880f7d5423e18252159e
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-12-16 15:40:33 +0300

    simple ui is done

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,4 +1,34 @@
-
+from tkinter import *
 
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+
+        # self.window.minsize(width=300, height=600)
+        self.window.config(padx=50, pady=50, bg=THEME_COLOR)
+
+        self.score = Label(text=f"Score: 0", bg=THEME_COLOR, fg="white")
+        self.score.grid(column=1, row=0, padx=20, pady=20)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="soru",
+            fill="black",
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        self.true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+        self.false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=self.false_image, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
