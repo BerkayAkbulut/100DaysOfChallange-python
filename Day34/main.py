29c8e32d4cf73bbe1e0f50ee85ab6f0c6b03e135
akbulutberkay <berkay.akbulut1@ogr.sakarya.edu.tr>
2022-12-16 10:57:38 +0300

    api is done

--- Day34/main.py
+++ Day34/main.py
@@ -1,17 +1,15 @@
 import requests
+import html
 
 from question_model import Question
-#from data import question_data
+from data import question_data
 from quiz_brain import QuizBrain
 
-question_data=requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
-question_data=question_data.json()
-print(question_data)
-
 question_bank = []
-for question in question_data["results"]:
+for question in question_data:
     #print(question)
     question_text = question["question"]
+    question_text = html.unescape(question_text)
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
